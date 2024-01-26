a20578ddd361c93aac5e73c008efb0e00dc5a0a6
fabio.furtado <fabio.furtado@youxgroup.com.br>
2024-01-26 12:53:34 -0300

    fix: Ajsute credenciais banco de dados

--- config/config.js
+++ config/config.js
@@ -1,20 +1,21 @@
-const { Pool } = require('pg');function createConnection() {
-  const pool = new Pool({
-    user: process.env.DBUSER,
-    host: process.env.HOSTNAME,
-    port: process.env.DBPORT,
-    database: process.env.DBNAME,
-    max: 20, 
-  });
+const { Pool } = require('pg');
+function createConnection() {
+  // const pool = new Pool({
+  //   user: process.env.DBUSER,
+  //   host: process.env.HOSTNAME,
+  //   port: process.env.DBPORT,
+  //   database: process.env.DBNAME,
+  //   max: 20, 
+  // });
 
-/*   const pool = new Pool({
+  const pool = new Pool({
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     host: process.env.HOSTNAME,
     port: process.env.DBPORT,
     database: process.env.DBNAME,
     max: 20, 
-  }); */
+  }); 
   return pool;
 }
 
